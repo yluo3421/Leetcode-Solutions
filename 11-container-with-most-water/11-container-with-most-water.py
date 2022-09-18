07965134449c1dea01cdb2c860ebe7bbadad8b3e
Yeming <102680280+yluo3421@users.noreply.github.com>
2022-09-17 20:41:48 -0400

    Time: 2125 ms (5.02%), Space: 26.2 MB (92.94%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,5 +1,11 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        # use two pointer from both sides
+        # if left pointer is shorter, move left +1
+        # if right pointer is shorter, move right -1
+        # if they are the same, move right -1
+        # use max to record max area
+        # area = min height of the two pointer * distance between two pointer
         l, r = 0, len(height) - 1
         res = 0
         while l < r:
