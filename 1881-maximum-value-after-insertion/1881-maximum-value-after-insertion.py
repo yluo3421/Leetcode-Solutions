587c1aa462a99e5dbdfa224f1ed98a1c7838806b
Yeming <102680280+yluo3421@users.noreply.github.com>
2022-09-22 11:06:52 -0400

    Time: 385 ms (21.99%), Space: 15.3 MB (41.13%) - LeetHub

---
+++ 1881-maximum-value-after-insertion/1881-maximum-value-after-insertion.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxValue(self, n: str, x: int) -> str:
+        check = (lambda i: str(x) > n[i]) if n[0] != '-' else (lambda i: str(x) < n[i])
+        for i in range(len(n)):
+            if check(i):
+                break
+        else:
+            i = len(n)
+        return n[:i] + str(x) + n[i:]
