4b1cac2e1e744d3cb3c4d6a10063289353c90161
Yeming <yluo3421@gmail.com>
2022-09-25 23:59:29 -0400

    solutiono updated

--- Python/378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
+++ Python/378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -2,7 +2,15 @@
 
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-
+        # use heap to record
+        # The heap data structure gives us O(1)O(1) access to the minimum 
+        # element and log(N)log(N) removal of the minimum element and addition 
+        # of a new one. We just need to perform this operation K times to get 
+        # our Kth smallest number. In this case, we can safely discard the rows 
+        # in the lower part of the matrix i.e. in this case starting from 
+        # the 6th row to the end of the matrix because the columns are sorted 
+        # as well. So, we need the min(N, K)min(N,K) rows essentially to 
+        # find out the answer.
         n = len(matrix)
         priorityqueue = [(matrix[i][0], i, 0) for i in range(n)]
         heapq.heapify(priorityqueue)
