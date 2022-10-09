58aeaf0c7d51dc5715d066fe5935466040af1266
Yeming <102680280+yluo3421@users.noreply.github.com>
2022-10-08 20:46:34 -0400

    Time: 2890 ms (23.81%), Space: 13.8 MB (89.80%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -0,0 +1,29 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        # thr brutal thoughts is to find palindrome from current char
+        # starting from the char to check all odd and even palindrome
+        # for odd and even, use two pointer to check
+        ans = s[0]
+        for i in range(0, len(s) - 1):
+            # odd
+            left, right = i, i
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if (right - left + 1) > len(ans):
+                    ans = s[left:right + 1]
+                left -= 1
+                right += 1
+            
+            # even
+            left, right = i, i + 1
+            while right < len(s) and s[left] == s[right] and left >= 0:
+                if (right - left + 1) > len(ans):
+                    ans = s[left:right + 1]
+                left -= 1
+                right += 1
+            
+            
+        return ans
+            
+                
+             
+        
