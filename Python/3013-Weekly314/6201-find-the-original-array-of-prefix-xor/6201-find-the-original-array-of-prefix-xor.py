3e6f30c99c424eff893334c51bd435c014458e3d
Yeming <yluo3421@gmail.com>
2022-10-09 00:01:16 -0400

    solution updated

---
+++ Python/3013-Weekly314/6201-find-the-original-array-of-prefix-xor/6201-find-the-original-array-of-prefix-xor.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findArray(self, pref: List[int]) -> List[int]:
+        # thoughts:
+        # 5 101
+        # 7 111
+        # 2  10
+        # 3  11
+        # 2  10
+        
+        # convert all number to binary and then compare the result
+        # ans[0] = pref[0]
+        # ans[1] = pref[1] / pref[0]
+        # ans[2] = pref[2] / 
+        # after trying several numbers realize that bitwise can be calculated reversed
+        ans = [pref[0]]
+        curr = pref[0]
+        for i in range(1, len(pref)):
+            curr ^= pref[i]
+            ans.append(curr)
+        return ans
+            
