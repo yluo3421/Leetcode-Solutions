bcc6f0d35accaee3b7498a0de12e2bcd60f4c3c0
Yeming <102680280+yluo3421@users.noreply.github.com>
2022-10-16 21:53:14 -0400

    Time: 269 ms (84.08%), Space: 24.1 MB (73.13%) - LeetHub

---
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -0,0 +1,39 @@
+class UndergroundSystem:
+
+    def __init__(self):
+        # checkInDict {id: (startStation, checkInTime)}
+        # tripDict {(startStation, endStation): [totalTime += checkOutTime - checkIntime, count]}
+        self.checkInDict = {}
+        self.tripDict = {}
+                       
+
+    def checkIn(self, id: int, stationName: str, t: int) -> None:
+        if id not in self.checkInDict:
+            self.checkInDict[id] = (stationName, t)
+        return
+        
+
+    def checkOut(self, id: int, stationName: str, t: int) -> None:
+        startStation, checkInTime = self.checkInDict[id]
+        del self.checkInDict[id]
+        if (startStation, stationName) not in self.tripDict:
+            self.tripDict[(startStation, stationName)] = [t - checkInTime, 1]
+        else:
+            self.tripDict[(startStation, stationName)][0] += (t - checkInTime)
+            self.tripDict[(startStation, stationName)][1] += 1
+        return
+        
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        if (startStation, endStation) not in self.tripDict:
+            return
+        else:
+            
+            totalTime = self.tripDict[(startStation, endStation)][0]
+            count = self.tripDict[(startStation, endStation)][1]
+            return totalTime/count
+
+# Your UndergroundSystem object will be instantiated and called as such:
+# obj = UndergroundSystem()
+# obj.checkIn(id,stationName,t)
+# obj.checkOut(id,stationName,t)
+# param_3 = obj.getAverageTime(startStation,endStation)
