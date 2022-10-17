55b3316bdbddc9af85aa850c3273688011ed151b
Yeming <102680280+yluo3421@users.noreply.github.com>
2022-10-16 22:08:41 -0400

    Time: 650 ms (14.73%), Space: 25.4 MB (31.93%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -10,12 +10,14 @@ class UndergroundSystem:
     def checkIn(self, id: int, stationName: str, t: int) -> None:
         if id not in self.checkInDict:
             self.checkInDict[id] = (stationName, t)
+        else:
+            self.checkInDict[id] = (stationName, t)
         return
         
 
     def checkOut(self, id: int, stationName: str, t: int) -> None:
         startStation, checkInTime = self.checkInDict[id]
-        del self.checkInDict[id]
+        # del self.checkInDict[id]
         if (startStation, stationName) not in self.tripDict:
             self.tripDict[(startStation, stationName)] = [t - checkInTime, 1]
         else:
