4a418b1efd8a947821c12797e751c0e7128b0539
Yeming <102680280+yluo3421@users.noreply.github.com>
2022-10-20 19:55:43 -0400

    Time: 80 ms (41.04%), Space: 14.1 MB (59.78%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,12 +1,11 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        self.dfs(nums, [], res)
-        return res
+        ans = []
+        self.dfs(nums, [], ans)
+        return ans
     
-    def dfs(self, nums, path, res):
+    def dfs(self, nums, path, ans):
         if not nums:
-            res.append(path)
-            return
+            ans.append(path)
         for i in range(len(nums)):
-            self.dfs(nums[:i]+nums[i+1:], path+[nums[i]], res)
+            self.dfs(nums[:i] + nums[i+1:], path + [nums[i]], ans)
