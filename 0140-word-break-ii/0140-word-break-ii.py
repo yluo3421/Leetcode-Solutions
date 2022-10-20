5ac50d711ce814b77dc5913087cf2ef1057a8dce
Yeming <102680280+yluo3421@users.noreply.github.com>
2022-10-20 15:41:18 -0400

    Time: 35 ms (87.97%), Space: 13.8 MB (81.63%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -0,0 +1,33 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        # dfs
+        # after choosing one word, the rest of the s can choose come different
+        # answer
+        
+        """
+        catsanddog
+        
+        cat     cats
+        sand    and
+        dog     dog
+        """
+        # backtracking
+        # the function checks if from i to j is in words
+        # if yes, append this word to path, and check from j position
+        # if it reaches end of s, append the joined path to ans
+        # if none of the word works, pop the last word from path
+        
+        def helper(i, path):
+            if i == len(s):
+                ans.append(" ".join(path))
+                return
+            for j in range(i + 1, len(s) + 1):
+                if s[i:j] in wordSet:
+                    path.append(s[i:j])
+                    helper(j, path)
+                    path.pop()
+        ans = []
+        wordSet = set(wordDict)
+        path = []
+        helper(0, path)
+        return ans
