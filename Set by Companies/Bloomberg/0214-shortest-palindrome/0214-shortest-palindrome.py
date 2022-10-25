f2bb9df9931ae3f9fc8f324a7ad9d1b535876d08
Yeming <yluo3421@gmail.com>
2022-10-25 14:54:21 -0400

    solution updated

--- Set by Companies/Bloomberg/0214-shortest-palindrome/0214-shortest-palindrome.py
+++ Set by Companies/Bloomberg/0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -15,3 +15,19 @@ class Solution:
             if substring == substring[::-1]:
                 suffix = s[i:]
                 return suffix[::-1] + s
+
+                
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        j=0
+        for i in range(len(s)-1,-1,-1):
+            if s[i]==s[j]:
+                j+=1
+        
+        if len(s)==j:
+            return s
+        # print("index j ",j)
+        suffix=s[j:]
+        # print("suffix ",suffix)
+        # print("reverse suffix ",suffix[::-1])
+        return suffix[::-1]+ self.shortestPalindrome(s[0:j])+suffix
