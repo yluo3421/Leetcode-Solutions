e0431ebea334043f31831efecd2d84e4d164347c
Yeming <102680280+yluo3421@users.noreply.github.com>
2022-10-28 22:17:06 -0400

    Time: 1291 ms (30.15%), Space: 15.3 MB (48.99%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        """
+        brutal force
+        i + nums[i] = True
+        """
+        last_position = len(nums) - 1
+        for i in range(len(nums) - 2, -1, -1):
+            if i + nums[i] >= last_position:
+                last_position = i
+        return last_position == 0
+                
+                    
