ec9a48045b9becdd7b1638f2a41c6f5201fe9aee
Yeming <102680280+yluo3421@users.noreply.github.com>
2022-10-30 21:55:29 -0400

    Time: 194 ms (57.07%), Space: 14.2 MB (67.74%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -0,0 +1,45 @@
+# Time: log(min(n, m))
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        A, B = nums1, nums2
+        total = len(nums1) + len(nums2) #3
+        half = total // 2 #1
+
+        if len(B) < len(A):
+            A, B = B, A
+            
+        l, r = 0, len(A) - 1 # 0,0
+        while True:
+            i = (l + r) // 2  # A #0
+            j = half - i - 1  # B  #-1
+
+            Aleft = A[i] if i >= 0 else float("-infinity")
+            Aright = A[i + 1] if (i + 1) < len(A) else float("infinity")
+            Bleft = B[j-1] if j-1 >= 0 else float("-infinity")
+            Bright = B[j] if (j) < len(B) else float("infinity")
+
+            # partition is correct
+            if Aleft <= Bright and Bleft <= Aright:
+                # odd
+                if total % 2:
+                    return min(Aright, Bright)
+                # even
+                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+            elif Aleft > Bright:
+                r = i - 1
+            else:
+                l = i + 1        
+        """
+        A=[1,,]
+        B=[3,4,]
+        half = (len(A) + len(B)) // 2 = 1
+        i = (0 + len(A) - 1)//2 = 0
+        j = half - i - 1 = 0
+          = (len(A) + len(B)) // 2 - (0 + len(A) - 1) // 2 - 2
+         ~ = (len(A) + len(B) - len(A) + 1 - ) // 2 - 1
+          = 
+        
+        
+        """
