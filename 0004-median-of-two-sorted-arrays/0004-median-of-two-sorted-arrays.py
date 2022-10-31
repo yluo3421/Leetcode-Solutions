8061bff0f85fc60a2a00fe59be36130a0ee4f09c
Yeming <102680280+yluo3421@users.noreply.github.com>
2022-10-31 19:35:54 -0400

    Time: 239 ms (17.48%), Space: 14.2 MB (67.77%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -3,43 +3,34 @@
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        # thoughts
+        # find mid point of A & B
+        # check if they can make the new array's left and right part
+        # cause median will fall on the end of two parts
         A, B = nums1, nums2
-        total = len(nums1) + len(nums2) #3
-        half = total // 2 #1
-
-        if len(B) < len(A):
+        total = len(A) + len(B)
+        half = total // 2
+        
+        if len(A) > len(B):
             A, B = B, A
-            
-        l, r = 0, len(A) - 1 # 0,0
+        left, right = 0 , len(A) - 1
         while True:
-            i = (l + r) // 2  # A #0
-            j = half - i - 1  # B  #-1
-
-            Aleft = A[i] if i >= 0 else float("-infinity")
-            Aright = A[i + 1] if (i + 1) < len(A) else float("infinity")
-            Bleft = B[j-1] if j-1 >= 0 else float("-infinity")
-            Bright = B[j] if (j) < len(B) else float("infinity")
-
-            # partition is correct
+            i = (left + right) // 2
+            j = half - i - 2
+            
+            Aleft = A[i] if i >= 0 else float("-inf")
+            Aright = A[i + 1] if (i + 1) < len(A) else float("inf")
+            Bleft = B[j] if j >= 0 else float("-inf")
+            Bright = B[j + 1] if (j + 1) < len(B) else float("inf")
+            
             if Aleft <= Bright and Bleft <= Aright:
-                # odd
-                if total % 2:
+                if total % 2 == 0:
+                    return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+                else:
                     return min(Aright, Bright)
-                # even
-                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
-            elif Aleft > Bright:
-                r = i - 1
+            elif Aright > Bright:
+                right = i - 1
             else:
-                l = i + 1        
-        """
-        A=[1,,]
-        B=[3,4,]
-        half = (len(A) + len(B)) // 2 = 1
-        i = (0 + len(A) - 1)//2 = 0
-        j = half - i - 1 = 0
-          = (len(A) + len(B)) // 2 - (0 + len(A) - 1) // 2 - 2
-         ~ = (len(A) + len(B) - len(A) + 1 - ) // 2 - 1
-          = 
-        
-        
-        """
+                left = i + 1
+            
+            
