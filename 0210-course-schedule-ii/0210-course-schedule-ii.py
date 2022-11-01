6f6a9c0d920adb6b8ac4fba11be57c3d5b54e0b5
Yeming <102680280+yluo3421@users.noreply.github.com>
2022-11-01 17:51:59 -0400

    Time: 131 ms (81.50%), Space: 17.4 MB (31.37%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,50 @@
+class Solution:
+    WHITE = 1 # not started
+    GRAY = 2 # checked
+    BLACK = 3 # finished
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # thoughts:
+        """
+        create a adj_list using defaultdict
+        mark all courses as WHITE
+        we will need to dfs all courses that is WHITE
+        inside dfs
+        we fist check if is_cycle, return
+        find all neighbors of the curr course, 
+        if one of the neighbor is gray, meaning cycle
+        otherwise mark all visited neighbors as GRAY
+        after loop mark curr as BLACK
+        append to the topological order
+        
+        after all nodes, return the reverse of the topological order
+        
+        """
+        adj_list = defaultdict(list)
+        for start, end in prerequisites:
+            adj_list[end].append(start)
+        
+        colors = {i: Solution.WHITE for i in range(numCourses)}
+        
+        topological_sorted_order = []
+        
+        is_cycle = False
+        
+        def dfs(node):
+            nonlocal is_cycle
+            if is_cycle:
+                return
+            
+            for neighbor in adj_list[node]:
+                if colors[neighbor] == Solution.GRAY:
+                    is_cycle = True
+                elif colors[neighbor] == Solution.WHITE:
+                    colors[neighbor] = Solution.GRAY
+                    dfs(neighbor)
+            colors[node] = Solution.BLACK
+            topological_sorted_order.append(node)
+        
+        for node in range(numCourses):
+            if colors[node] == Solution.WHITE:
+                dfs(node)
+        return topological_sorted_order[::-1] if not is_cycle else []
+        
