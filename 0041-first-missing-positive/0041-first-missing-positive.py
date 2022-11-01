4fc7b20e742541565380a98f4aee29793fbcd11c
Yeming <102680280+yluo3421@users.noreply.github.com>
2022-10-31 22:32:24 -0400

    Time: 390 ms (77.70%), Space: 27.2 MB (58.00%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -2,17 +2,18 @@ class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         # set could do O(n) space
         # but this swap sort could do O(1) space
-       
-    
         n = len(nums)
         i = 0
         while i < n:
             if nums[i] > 0 and nums[i] <= n and nums[i] != nums[nums[i] - 1]:
-                nums[nums[i] - 1], nums[i]= nums[i], nums[nums[i] - 1]
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
             else:
                 i += 1
+        
         for i in range(len(nums)):
             if nums[i] != i + 1:
-                return (i + 1)
+                return i + 1
         return n + 1
+    
+        
         
