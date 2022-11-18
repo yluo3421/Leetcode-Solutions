b36acf14a9823e39a92955eb8bc060227664785a
Yeming <yluo3421@gmail.com>
2022-11-18 15:59:37 -0500

    solution updated

--- Set by Companies/Bloomberg/0377-combination-sum-iv/0377-combination-sum-iv.py
+++ Set by Companies/Bloomberg/0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -1,9 +1,15 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
+        # use a dp to do this
+        # dp[i] represents number of combinations that can add to target
+        # for all num in nums
+        # dp[i] += dp[i - num]
+        # number of combination for dp[0] = 1
         dp = [0] * (target + 1)
         dp[0] = 1
         for i in range(1, target + 1):
             for num in nums:
                 if num <= i:
                     dp[i] += dp[i - num]
+        print(dp)
         return dp[target]
