9d454c9030eae55a59b1a2561478326a3e5a6195
Yeming <yluo3421@gmail.com>
2022-11-21 16:50:01 -0500

    jiuzhang updated

---
+++ Set by Companies/Bloomberg/0235_lowest_common_ancestor_of_binary_serach_tree/0235_lowest_common_ancestor_of_binary_serach_tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        curr = root
+        # if p.val < root.val and q.val < root.val
+        # root needs to go one level further
+        while curr:
+            if p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+            elif p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+            else:
+                break
+        return curr
+        
