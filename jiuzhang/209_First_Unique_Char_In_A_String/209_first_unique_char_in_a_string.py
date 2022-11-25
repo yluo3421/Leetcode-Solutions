13f4e6f2e2999ef4c6d0fc52f3ae06010926a0a9
Yeming <yluo3421@gmail.com>
2022-11-25 11:56:29 -0500

    jiuzhang updated

---
+++ jiuzhang/209_First_Unique_Char_In_A_String/209_first_unique_char_in_a_string.py
@@ -0,0 +1,34 @@
+class Solution:
+    """
+    @param str: str: the given string
+    @return: char: the first unique character in a given string
+    """
+    def first_uniq_char(self, str: str) -> str:
+        # Write your code here
+        # method 2
+        position = dict()
+        q = collections.deque()
+        n = len(str)
+        for i, ch in enumerate(str):
+            if ch not in position:
+                position[ch] = i
+                q.append((str[i], i))
+            else:
+                position[ch] = -1
+                while q and position[q[0][0]] == -1:
+                    q.popleft()
+        return ' ' if not q else q[0][0]
+
+
+        # go through the array and use a set to record
+        freq_dict = collections.Counter(str)
+        for char in str:
+            if freq_dict[char] == 1:
+                return char
+        
+        
+            
+            
+        
+        
+        
