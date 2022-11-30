83fea6429c9cc9fd2c6f343d9be06d6704e9303d
Yeming <yluo3421@gmail.com>
2022-11-30 16:49:41 -0500

    solution updated

--- Set by Companies/Bloomberg/1169-invalid-transactions/1169-invalid-transactions.py
+++ Set by Companies/Bloomberg/1169-invalid-transactions/1169-invalid-transactions.py
@@ -1,4 +1,46 @@
 class Solution:
+    def invalidTransactions(self, transactions: List[str]) -> List[str]:
+        # thoughts:
+        # push all names into a dict, with transactions as their value
+        # check each name's transaction if they are valid
+        # push invalid ones to ans array
+        # the first idea is to use dict store names with transaction idx
+        # but this is complicated when it comes to same name different city
+        # new idea is to use a default dict of default dict
+        cities = defaultdict(lambda: defaultdict(list))
+        output = []
+        
+		#build city map. 
+        for t in transactions:
+            name, time, amount, city = t.split(',')
+            cities[city][name].append(time)
+
+		#Check each transaction against all transactions in a given city name from a given person
+        for t in transactions:
+            name, time, amount, city = t.split(',')
+            
+			#Case 1
+            if int(amount) > 1000:
+                output.append(t)
+                continue
+                
+			#Case 2
+            for k,v in cities.items():
+                if k == city:
+                    continue
+                    
+                if any([abs(int(x) - int(time)) <= 60 for x in v[name]]):
+                    output.append(t)
+                    break;
+                
+        
+        return output
+        
+            
+
+    
+    
+class Solution:
 #     def invalidTransactions(self, transactions: List[str]) -> List[str]:
 #         """
 #         go through transactions and find those with same name,
@@ -29,33 +71,5 @@ class Solution:
                 
                     
 #         return ans  
-    def invalidTransactions(self, transactions: List[str]) -> List[str]:
-        cities = defaultdict(lambda: defaultdict(list))
-        output = []
-        
-		#build city map. 
-        for t in transactions:
-            name, time, amount, city = t.split(',')
-            cities[city][name].append(time)
 
-		#Check each transaction against all transactions in a given city name from a given person
-        for t in transactions:
-            name, time, amount, city = t.split(',')
-            
-			#Case 1
-            if int(amount) > 1000:
-                output.append(t)
-                continue
-                
-			#Case 2
-            for k,v in cities.items():
-                if k == city:
-                    continue
-                    
-                if any([abs(int(x) - int(time)) <= 60 for x in v[name]]):
-                    output.append(t)
-                    break;
-                
-        
-        return output
                         
