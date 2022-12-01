92e4c2205f3e0c0b50fce91cd92e57c30261cf5b
Yeming <yluo3421@gmail.com>
2022-12-01 15:01:09 -0500

    solution completed

--- Set by Companies/Bloomberg/1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ Set by Companies/Bloomberg/1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,22 +1,34 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        # thoughts
-        # use dict to record both string
-        # add up absolute of diff of each char in both dict
-        # then divided by 2
+        """
+        thoughts:
+        anagram are two strings with not only same chars
+        but also same amount of each char
+        To check if they are anagram, I can put all chars
+        into two dicts of the two words
+        If they are the same then its anagram and return 0
+        If not, I will calculate the difference and that's amount 
+        of chars needs to be changed
+        Let's use example 2 to do a dry run
+        leetcode = {c:1, d:1, e:3, l:1, o:1, t:1 }
+        practice = {a:1, c:2, e:1, i:1, p:1, r:1, t:1}
+        It is hard to compare in this way, I want to make sure both dict
+        has the same keys.
+        Here's my plan, I will find out whoever not in s_dict and add
+        them in there with value of 0. Same thing to the t_dict.
+        Then I will count the diff
+        Time complexity of this method will be O(m + n) with length of 
+        two strings, Space O(m + n) if they are totally different words
+        Is this what you are looking for?
+        """
         char_s, char_t = {}, {}
         for char in s:
             char_s[char] = char_s.get(char, 0) + 1
+            char_t[char] = char_t.get(char, 0)
         for char in t:
             char_t[char] = char_t.get(char, 0) + 1
-        for char in t:
-            if char not in s:
-                char_s[char] = 0
-        for char in s:
-            if char not in t:
-                char_t[char] = 0
-        ans = 0       
+            char_s[char] = char_s.get(char, 0)
+        ans = 0
         for char in char_s.keys():
             ans += abs(char_s[char] - char_t[char])
         return int(ans/2)
-        
