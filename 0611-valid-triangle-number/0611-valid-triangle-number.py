ba853aa91be55c68dab749159923eb9622639612
Yeming <102680280+yluo3421@users.noreply.github.com>
2022-12-02 16:44:02 -0500

    Time: 9983 ms (5.00%), Space: 14 MB (29.51%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.py
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -1,7 +1,23 @@
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
         """
+        thoughts:
+        the brutal force is to go through nums by three times
+        find three numbers and make sure they can form a triangle
+        That will be O(n^3)
+        To optimize this, I think I can sort the source array
+        first, this will give me chance to use binary serach.
+        Which will improve it to O(n^2log(n))
+        Because its sorted, if we found pair (i,j)
+        The third one we found that nums[k] > nums[i] + nums[j]
+        everyting betweein nums[j+1:k] are all answers
+        we can add total count by k - j - 1
         
+        Loop of k and j will be executed O(n^2) times in total, 
+        because, we do not reinitialize the value of k
+        for a new value of j
+        chosen(for the same i). Thus the complexity will be 
+        O(nlogn + n^2)
         """
         nums.sort()
         count = 0
