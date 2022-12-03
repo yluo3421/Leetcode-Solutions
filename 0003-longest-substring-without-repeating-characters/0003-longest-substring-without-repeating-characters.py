de3522cd432082b56fe1078c8facd97544822adb
Yeming <102680280+yluo3421@users.noreply.github.com>
2022-12-03 14:16:29 -0500

    Time: 168 ms (35.54%), Space: 14 MB (93.67%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,7 +4,18 @@ class Solution:
         Thoughts:
         The brutal force is to check all chars
         That will take about O(n^3) time
-        
+        I want to use a sliding window method,
+        that right pointer will move when no duplicates
+        if there is a duplicate, left will start to move
+        until right pointer char count reduce to 1
+        This way I can do it in O(n) because the two pointer
+        are moving seperately.
+        The worst case is O(2n) where each char visited by
+        left and right pointer.
+        Space O(min(m, n))
+        window needs O(k) where k is size of dict
+        dict has upper bound of size of string size n and
+        size of charset/alphabet m.
         """
         chars = defaultdict(int)
         left, right = 0, 0
