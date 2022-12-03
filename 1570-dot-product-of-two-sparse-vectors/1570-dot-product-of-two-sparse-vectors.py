2560fec0435ad287a76737c8b410f524a7e2fe01
Yeming <102680280+yluo3421@users.noreply.github.com>
2022-12-03 15:40:37 -0500

    Time: 1852 ms (85.70%), Space: 18.9 MB (14.76%) - LeetHub

--- 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.py
+++ 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.py
@@ -4,14 +4,42 @@ class SparseVector:
         Thoughts:
         Store the nums as it is
         Then calculate the sum of product
+        To better perform, because 0 times any number is 0
+        we can save some calculation
+        We can also store the sparse vecotr as a list of
+        (idx, value) pairs.
+        Then we can use two pointers to calculate the dot 
+        product.
         """
-        self.vector = nums
+        # brutal method
+        # self.vector = nums
+        self.pairs = []
+        for idx, value in enumerate(nums):
+            if value != 0:
+                self.pairs.append([idx, value])
 
     # Return the dotProduct of two sparse vectors
     def dotProduct(self, vec: 'SparseVector') -> int:
+        # brutal method
+        # ans = 0
+        # for i, j in zip(self.vector, vec.vector):
+        #     if i == 0 or j == 0:
+        #         continue
+        #     ans += i * j
+        # return ans
         ans = 0
-        for i, j in zip(self.vector, vec.vector):
-            ans += i * j
+        left, right = 0, 0
+        n, m = len(self.pairs), len(vec.pairs)
+        
+        while left < n and right < m:
+            if self.pairs[left][0] == vec.pairs[right][0]:
+                ans += self.pairs[left][1] * vec.pairs[right][1]
+                left += 1
+                right += 1
+            elif self.pairs[left][0] < vec.pairs[right][0]:
+                left += 1
+            else:
+                right += 1
         return ans
 
 # Your SparseVector object will be instantiated and called as such:
