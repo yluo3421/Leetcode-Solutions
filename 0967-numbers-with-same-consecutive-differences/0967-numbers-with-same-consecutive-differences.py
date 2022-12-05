ae1f59fcc7ec8e46ebabcf72ca6f2b18e9267936
Yeming <102680280+yluo3421@users.noreply.github.com>
2022-12-05 18:02:41 -0500

    Time: 71 ms (65.52%), Space: 14.3 MB (18.91%) - LeetHub

--- 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.py
+++ 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.py
@@ -1,5 +1,14 @@
 class Solution:
     def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
+        """
+        The exit of this dfs is that n == 0, that we have no digits to 
+        fill in.
+        Based on the last digit of the current number
+        Lets call is tail_digit
+        We can add difference of k to it, or minus from it
+        If the next_digit is valid digit (0-9)
+        We dfs to the next level.
+        """
         if n == 1:
             return [i for i in range(10)]
         ans = []
