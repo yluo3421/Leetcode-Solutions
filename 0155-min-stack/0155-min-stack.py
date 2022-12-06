aee0efc8a3b9eb1785e8d7ffaf8a11c05ab65787
Yeming <102680280+yluo3421@users.noreply.github.com>
2022-12-06 17:32:05 -0500

    Time: 110 ms (69.31%), Space: 18 MB (57.67%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.py
@@ -0,0 +1,34 @@
+class MinStack:
+
+    def __init__(self):
+        self.minstack = []
+        self.stack = []
+        
+
+    def push(self, val: int) -> None:
+        if not self.minstack:
+            self.minstack.append(val)
+        else:
+            if val < self.minstack[-1]:
+                self.minstack.append(val)
+            else:
+                self.minstack.append(self.minstack[-1])
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minstack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minstack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
