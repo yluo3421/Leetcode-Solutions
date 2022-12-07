966512cd82b77a8cd3613602331d79018a16984c
Yeming <102680280+yluo3421@users.noreply.github.com>
2022-12-07 13:30:19 -0500

    Time: 2075 ms (64.70%), Space: 18.1 MB (20.22%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
@@ -1,11 +1,31 @@
 class Solution:
     def minCost(self, n: int, cuts: List[int]) -> int:
-        cuts.extend([0, n]); 
-        cuts.sort(); 
-        m = len(cuts)        
-        dp = [[0] * m for _ in range(m)]
-        for l in range(2, m):
-            for i in range(m-l):
-                j = i + l                        
-                dp[i][j] = min(cuts[j] - cuts[i] + dp[i][k] + dp[k][j] for k in range(i+1, j))
-        return dp[0][-1]
+        """
+        Perform Brute Force Recursion
+        1. At any state of recursion, we would be having 
+        a piece of rod (or wood), from index l to index r.
+        2. Try all the possible cuts on this segment, which 
+        splits it into further 2 segments and recurse for 
+        the minimum cost in which those 2 segments can be made.
+
+        3. The minimum cost among all those possible cuts + the 
+        cost of making our current cut, would be our best cost 
+        for cutting the wood in this segment, i.e. from l to r.
+        
+        4. Memoize the minimum cost for the current l and r value in a memo.
+            If the answer for l to r was already present in 
+            our memo, then directly retrun the value stored 
+            in the memo corresponding to this l and r instead of 
+            performing from step 1.
+        """
+        cuts.extend([0, n])  # add 2 fake cuts as the boundary of the first cut
+        cuts.sort()
+        @functools.lru_cache(None)
+        def f(i, j):
+            if i + 1 >= j: return 0
+            """
+            cuts[j] - cuts[i] # cost of the first cut between ith and jth cut
+                + min((f(i, k) + f(k, j) for k in range(i+1, j)), default=0)  # go through all the cuts as the first cut
+            """
+            return cuts[j] - cuts[i] + min((f(i, k) + f(k, j) for k in range(i+1, j)), default=0)
+        return f(0, len(cuts)-1)
