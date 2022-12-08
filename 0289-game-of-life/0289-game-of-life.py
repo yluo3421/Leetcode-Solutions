77c10734812d65f51db9dc4dabbd73b2d6a0d66e
Yeming <yluo3421@gmail.com>
2022-12-07 19:06:15 -0500

    solution completed

--- 0289-game-of-life/0289-game-of-life.py
+++ 0289-game-of-life/0289-game-of-life.py
@@ -2,6 +2,11 @@ class Solution:
     def gameOfLife(self, board: List[List[int]]) -> None:
         """
         Do not return anything, modify board in-place instead.
+        
+        Make a copy of the original board which will remain unchanged throughout the process.
+Iterate the cells of the Board one by one.
+While computing the results of the rules, use the copy board and apply the result in the original board.
+
         """
         # Neighbors array to find 8 neighboring cells for a given cell
         neighbors = [(1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1), (0,1), (1,1)]
@@ -34,3 +39,72 @@ class Solution:
                 # Rule 4
                 if copy_board[row][col] == 0 and live_neighbors == 3:
                     board[row][col] = 1
+        """
+        O(1) Space
+        
+        
+        """
+        class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        Iterate the cells of the Board one by one.
+
+The rules are computed and applied on the original board. The updated values signify both previous and updated value.
+
+The updated rules can be seen as this:
+
+Rule 1: Any live cell with fewer than two live neighbors dies, as if caused by under-population. Hence, change the value of cell to -1. This means the cell was live before but now dead.
+
+Rule 2: Any live cell with two or three live neighbors lives on to the next generation. Hence, no change in the value.
+
+Rule 3: Any live cell with more than three live neighbors dies, as if by over-population. Hence, change the value of cell to -1. This means the cell was live before but now dead. Note that we don't need to differentiate between the rule 1 and 3. The start and end values are the same. Hence, we use the same dummy value.
+
+Rule 4: Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction. Hence, change the value of cell to 2. This means the cell was dead before but now live.
+
+Apply the new rules to the board.
+
+Since the new values give an indication of the old values of the cell, we accomplish the same results as approach 1 but without saving a copy.
+
+To get the Board in terms of binary values i.e. live(1) and dead(0), we iterate the board again and change the value of a cell to a 1 if its value currently is greater than 0 and change the value to a 0 if its current value is lesser than or equal to 0.
+
+
+        """
+        # Neighbors array to find 8 neighboring cells for a given cell
+        neighbors = [(1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1), (0,1), (1,1)]
+
+        rows = len(board)
+        cols = len(board[0])
+
+        # Iterate through board cell by cell.
+        for row in range(rows):
+            for col in range(cols):
+
+                # For each cell count the number of live neighbors.
+                live_neighbors = 0
+                for neighbor in neighbors:
+
+                    # row and column of the neighboring cell
+                    r = (row + neighbor[0])
+                    c = (col + neighbor[1])
+
+                    # Check the validity of the neighboring cell and if it was originally a live cell.
+                    if (r < rows and r >= 0) and (c < cols and c >= 0) and abs(board[r][c]) == 1:
+                        live_neighbors += 1
+
+                # Rule 1 or Rule 3
+                if board[row][col] == 1 and (live_neighbors < 2 or live_neighbors > 3):
+                    # -1 signifies the cell is now dead but originally was live.
+                    board[row][col] = -1
+                # Rule 4
+                if board[row][col] == 0 and live_neighbors == 3:
+                    # 2 signifies the cell is now live but was originally dead.
+                    board[row][col] = 2
+
+        # Get the final representation for the newly updated board.
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col] > 0:
+                    board[row][col] = 1
+                else:
+                    board[row][col] = 0
