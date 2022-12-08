e30905f4d3fb91b03a1d4c63818d7bb0da97f9d9
Yeming <102680280+yluo3421@users.noreply.github.com>
2022-12-07 21:12:45 -0500

    Time: 2239 ms (5.27%), Space: 20.3 MB (42.11%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
@@ -4,9 +4,26 @@ class Solution:
         dp[0] = largest sum which is divisible by 3
         dp[1] = largest sum when divided by 3, remainder = 1
         dp[2] = largest sum when divided by 3, remainder = 2
+        [3,6,5,1,8]
+        [0, 0, 0]
+        [3, 0, 0]
+        [3, 0, 0]
+        [3, 0, 0]
+        [9, 0, 0]
+        [9, 0, 0]
+        [9, 0, 0]
+        [9, 0, 14]
+        [9, 0, 14]
+        [9, 0, 14]
+        [9, 10, 14]
+        [9, 10, 14]
+        [15, 10, 14]
+        [15, 10, 23]
+        [18, 10, 23]
         """
         dp = [0, 0, 0]
         for num in nums:
-            for i in dp[:]:
+            for i in dp[:]: # dp[:] makes a shallow copy of dp
+                print(dp)
                 dp[(i + num) % 3] = max(dp[(i + num) % 3], i + num)
         return dp[0]
