87dc6026c63db4717110dd126191cf3475b40d72
Yeming <102680280+yluo3421@users.noreply.github.com>
2022-12-08 21:59:24 -0500

    Time: 1037 ms (12.87%), Space: 34.6 MB (15.28%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        rank = {}
+        for a in sorted(arr):
+            rank.setdefault(a, len(rank) + 1)
+        return map(rank.get, arr)
