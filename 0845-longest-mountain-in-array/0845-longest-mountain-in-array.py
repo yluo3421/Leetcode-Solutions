54b35c10f38e745b0c9a7a345a4d3fd2c912f8ff
Yeming <102680280+yluo3421@users.noreply.github.com>
2022-12-08 22:03:26 -0500

    Time: 395 ms (52.08%), Space: 15.1 MB (81.05%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
@@ -1,5 +1,15 @@
 class Solution:
     def longestMountain(self, arr: List[int]) -> int:
+        """
+        For a starting index base, let's calculate the length of the longest mountain A[base], A[base+1], ..., A[end].
+
+If such a mountain existed, the next possible mountain will start at base = end; if it didn't, then either we reached the end, or we have A[base] >= A[base+1] and we can start at base + 1.
+
+Here is a worked example on the array A = [1, 2, 3, 2, 1, 0, 2, 3, 1]:
+base starts at 0, and end travels using the first while loop to end = 2 (A[end] = 3), the potential peak of this mountain. After, it travels to end = 5 (A[end] = 0) during the second while loop, and a candidate answer of 6 (base = 0, end = 5) is recorded.
+
+Afterwards, base is set to 5 and the process starts over again, with end = 7 the peak of the mountain, and end = 8 the right boundary, and the candidate answer of 4 (base = 5, end = 8) being recorded.
+        """
         N = len(arr)
         ans = base = 0
 
