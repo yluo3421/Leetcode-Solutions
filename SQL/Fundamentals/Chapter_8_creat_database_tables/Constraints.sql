90273ff785a165b5b24941b59f041f2f75d45781
Yeming <yluo3421@gmail.com>
2023-02-22 20:42:03 -0500

    CRUD

---
+++ SQL/Fundamentals/Chapter_8_creat_database_tables/Constraints.sql
@@ -0,0 +1,19 @@
+/*
+Column constraints
+Table constraints
+*/
+
+NOT NULL;
+-- email
+UNIQUE;
+--
+PRIMARY KEY;
+-- Uniquely identifies each row-record in a database table
+FOREIGN KEY;
+-- Constrains data based on columns in other tables
+CHECK;
+-- Ensures that all values in a column satisfy certain conditions
+EXCLUSION;
+-- Ensures that if any two rows are compared on the specified
+-- column or expression.
+
---
+++ SQL/Fundamentals/Chapter_8_creat_database_tables/Data_Types.sql
@@ -0,0 +1,29 @@
+
+/*
+True False
+Char, Varcahr, text
+Int, floating point number
+date, time, timestamp
+UUID Universally Unique Identifiers
+Array
+JSON
+Hstore key-value pairs
+*/
+
+/*
+smallint
+int
+bigiint
+decimal
+numeric
+real
+double prevision
+smallserial
+serial
+
+What shall we use to store phone numbers
+Why bother with numerics at all?
+We dont perform arithmetic with numbers so it probably makes more sense as a VARCHAR data type
+
+*/
+
