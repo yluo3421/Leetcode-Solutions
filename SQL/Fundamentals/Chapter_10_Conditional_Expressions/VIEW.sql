2a39752c88a55660ba3c7287b55afbcb84ea8888
Yeming <yluo3421@gmail.com>
2023-02-26 11:43:07 -0500

    VIEW CRUD

--- SQL/Fundamentals/Chapter_10_Conditional_Expressions/VIEW.sql
+++ SQL/Fundamentals/Chapter_10_Conditional_Expressions/VIEW.sql
@@ -16,3 +16,6 @@ CREATE OR REPLACE VIEW customer_info AS
 SELECT first_name, last_name, address, district FROM customer
 INNER JOIN address 
 ON customer.address_id = address.address_id;
+
+DROP VIEW IF EXISTS customer_info;
+ALTER VIEW customer_info RENAME TO c_info;
