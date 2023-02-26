edb3b2111fc4a5927e8217115ecf1bb50fe90819
Yeming <yluo3421@gmail.com>
2023-02-26 11:41:59 -0500

    updated

--- SQL/Fundamentals/Chapter_10_Conditional_Expressions/VIEW.sql
+++ SQL/Fundamentals/Chapter_10_Conditional_Expressions/VIEW.sql
@@ -9,3 +9,10 @@ ON customer.address_id = address.address_id;
 
 SELECT * FROM customer_info;
 -- will return the same thing.
+
+
+-- to alter
+CREATE OR REPLACE VIEW customer_info AS 
+SELECT first_name, last_name, address, district FROM customer
+INNER JOIN address 
+ON customer.address_id = address.address_id;
